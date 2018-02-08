0efa97345bd9bfd5ee83e2b4b347a7cfca549cf8
andychow122 <andychow122@gmail.com>
2018-02-08 00:20:02 -0700

    heroku

--- server.js
+++ server.js
@@ -32,10 +32,20 @@ app.use(express.static("public"));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// // Set mongoose to leverage built in JavaScript ES6 Promises
+// // Connect to the Mongo DB
+// mongoose.Promise = Promise;
+// mongoose.connect("mongodb://localhost/scraperHW", {
+//   useMongoClient: true
+// });
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/scraperHW", {
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
